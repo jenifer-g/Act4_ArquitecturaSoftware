1fc0b9794b365dde4dd881b9fbd7695c49a443d9
Jennifer <22735@virtual.utsc.edu.mx>
2025-10-17 16:31:36 -0600

    Se agrego el modo oscuro a la pagina

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 document.getElementById('contacto').addEventListener('submit', ()=>{
     alert("Correo enviado, en un momentos nos ponemos en contacto!")
 });
+
+
+document.getElementById('tema').addEventListener('click', ()=>{
+    const header = document.querySelector('header');
+    const titulos2 = document.querySelectorAll('.titulo2');
+    const general = document.querySelector('.general');
+    const p_sobremi = document.querySelector('.sobremi');
+    const body = document.querySelector('body');
+
+    body.classList.toggle('oscuro');
+    p_sobremi.classList.toggle('oscuro')
+    header.classList.toggle('oscuro');
+    general.classList.toggle('oscuro');
+
+    titulos2.forEach(t=> {
+        t.classList.toggle('oscuro');
+    })
+});
